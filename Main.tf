1194f6b32003b9d92d495c76b300b33901494246
raghunandanpattar <53566456+raghunandanpattar@users.noreply.github.com>
2023-04-16 14:28:47 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,5 +1,7 @@
 provider "aws"{
     region ="us-east-1"
+    access_key ="AKIAYBUDLFUTY4WVDZGC"
+    secret_key ="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"
 }
 resource "aws_s3_bucket" "my_first_bucket" {
     bucket = "my-first-bucket-in28mins-123456789"
