e444c91e8946ff95dfb93af3ac0c683e0d1ec420
raghunandanpattar <53566456+raghunandanpattar@users.noreply.github.com>
2023-04-16 15:43:21 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -7,3 +7,34 @@ resource "aws_s3_bucket" "my_first_bucket" {
     bucket = "my-first-bucket-in28mins-123456789"
 }
 
+
+
+resource "aws_security_group" "task-sg" {
+  name="task-sg"
+  vpc_id = "vpc-05c8d369712b48906"
+
+  ingress{
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress{
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+}
+
+resource "aws_instance" "task-instance" {
+  ami           = "ami-06e46074ae430fba6"
+  instance_type = "t2.micro"
+  key_name      = "default-ec2"
+  vpc_security_group_ids = [aws_security_group.task-sg.id]
+
+}
+
