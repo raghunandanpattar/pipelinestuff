b5d2bf336c7b753bbfd9c0ec3bcb9085117236e8
raghunandanpattar <53566456+raghunandanpattar@users.noreply.github.com>
2023-04-16 15:37:04 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -6,7 +6,4 @@ provider "aws"{
 resource "aws_s3_bucket" "my_first_bucket" {
     bucket = "my-first-bucket-in28mins-123456789"
 }
-resource "aws_iam_user" "first_iam_user" {
-    name="first_iam_user_raghu_updated1234"
-  
-}
+
