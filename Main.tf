00f808dae69086e0fa15107a41db27d2a970f1a2
raghunandanpattar <53566456+raghunandanpattar@users.noreply.github.com>
2023-04-17 14:04:42 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,7 +1,7 @@
 provider "aws"{
     region ="us-east-1"
-    access_key ="AKIAYBUDLFUTTN4CCEFM"
-    secret_key ="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"
+    access_key ="AKIAYBUDLFUTXQXVMQ46"
+    secret_key ="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"
 }
 resource "aws_s3_bucket" "my_final_bucket" {
     bucket = "final-bucket89511"
