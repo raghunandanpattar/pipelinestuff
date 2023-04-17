2529ba0cc7c792a3a48a090365d07bba3c7d71c3
raghunandanpattar <53566456+raghunandanpattar@users.noreply.github.com>
2023-04-17 18:31:55 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -9,5 +9,34 @@ resource "aws_s3_bucket" "my_final_bucket" {
 
 
 
+resource "aws_security_group" "raghu_sg" {
+  name   = "raghu_sg"
+  vpc_id = "vpc-05c8d369712b48906"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  tags = {
+    "name" = "raghus_sg"
+  }
+
+}
 
 
