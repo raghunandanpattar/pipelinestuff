052f93cee7d91ac35ca5bf8f8dd2ca06ad3c5583
raghunandanpattar <53566456+raghunandanpattar@users.noreply.github.com>
2023-04-17 15:28:21 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -11,11 +11,3 @@ resource "aws_s3_bucket" "my_final_bucket" {
 
 
 
-#resource "aws_instance" "task-instance" {
- # ami           = "ami-06e46074ae430fba6"
-  #instance_type = "t2.micro"
-  #key_name      = "default-ec2"
-  #vpc_security_group_ids = [aws_security_group.task-sg.id]
-
-#}
-
