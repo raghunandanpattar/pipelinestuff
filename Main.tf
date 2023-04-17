f1492adf9ee2f10dfff7257be88c17075f2be2ab
raghunandanpattar <53566456+raghunandanpattar@users.noreply.github.com>
2023-04-17 13:59:17 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -3,32 +3,13 @@ provider "aws"{
     access_key ="AKIAYBUDLFUTY4WVDZGC"
     secret_key ="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"
 }
-resource "aws_s3_bucket" "my_first_bucket" {
-    bucket = "my-first-bucket-in28mins-123456789"
+resource "aws_s3_bucket" "my_final_bucket" {
+    bucket = "final-bucket89511"
 }
 
 
 
-resource "aws_security_group" "task-sg" {
-  name="task-sg"
-  vpc_id = "vpc-05c8d369712b48906"
 
-  ingress{
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-
-  egress{
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-}
 
 #resource "aws_instance" "task-instance" {
  # ami           = "ami-06e46074ae430fba6"
