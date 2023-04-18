e30976f8f6baca53eccf3d5faf72fe2e4ca6c3e5
raghunandanpattar <53566456+raghunandanpattar@users.noreply.github.com>
2023-04-18 11:02:05 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -38,5 +38,9 @@ resource "aws_security_group" "raghunandan_sg" {
   }
 
 }
-
-
+resource "aws_instance" "task-instance" {
+  ami           = "ami-06e46074ae430fba6"
+  instance_type = "t2.micro" 
+  vpc_security_group_ids = [aws_security_group.raghunandan_sg.id]
+  
+}
