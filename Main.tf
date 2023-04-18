274efb7df9731b8fb92b5a05a20b522be3d0804f
raghunandanpattar <53566456+raghunandanpattar@users.noreply.github.com>
2023-04-18 12:58:10 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -3,14 +3,14 @@ provider "aws"{
     access_key ="AKIAYBUDLFUT4G62IWY4"
     secret_key ="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"
 }
-resource "aws_s3_bucket" "my_final_bucket" {
-    bucket = "final-bucket89511"
+resource "aws_s3_bucket" "my_final_bucket-1" {
+    bucket = "final-bucket89511-1"
 }
 
 
 
-resource "aws_security_group" "raghunandan_sg" {
-  name   = "raghunandan_sg"
+resource "aws_security_group" "raghunandan_final-sg" {
+  name   = "raghunandan_final-sg"
   vpc_id = "vpc-05c8d369712b48906"
   ingress {
     from_port   = 80
@@ -42,7 +42,7 @@ resource "aws_instance" "task-instance11" {
   ami           = "ami-06e46074ae430fba6"
   instance_type = "t2.micro" 
   key_name               = "default-ec2"
-  vpc_security_group_ids = [aws_security_group.raghunandan_sg.id]
+  vpc_security_group_ids = [aws_security_group.raghunandan_final-sg.id]
   subnet_id = "subnet-0a97bed60f00da0be"
     
   tags ={
